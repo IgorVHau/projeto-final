50aaf62bdfdacd8606cd98f29da48668ab27e0b5
brunodias52 <brunodias52@hotmail.com>
2021-12-10 22:59:33 -0300

    Resolvendo problemas de merge do commit anterior

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/Pages/Home/Home';
 import Login from './src/Pages/Login'
-=======
-import * as React from 'react';
 import AppRoutes from './routes/app.routes';
->>>>>>> 78c6e2ed23ba2ecdf86edb187c16788a38c39257
+
 
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
       <NavigationContainer>
         {/* <Home/> */}
         <Login/>
      </NavigationContainer>
-=======
         <AppRoutes />
->>>>>>> 78c6e2ed23ba2ecdf86edb187c16788a38c39257
     </>
 );
 }
